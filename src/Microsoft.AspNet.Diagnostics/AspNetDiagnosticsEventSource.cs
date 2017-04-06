fab1bde530695c81f58299df52b170f8834956e5
Matt Fei <mattfei@microsoft.com>
2017-04-06 09:55:31 -0700

    revise EventSource code and update setup proj

--- src/Microsoft.AspNet.Diagnostics/AspNetDiagnosticsEventSource.cs
+++ src/Microsoft.AspNet.Diagnostics/AspNetDiagnosticsEventSource.cs
@@ -6,7 +6,7 @@ namespace Microsoft.AspNet.Diagnostics
     /// <summary>
     /// ETW EventSource tracing class.
     /// </summary>
-    [EventSource(Name = "Microsoft-AspNet-Diagnostics")]
+    [EventSource(Name = "Microsoft-AspNet-Diagnostics", Guid = "B72742AA-ED2D-407A-ABA3-9DF2522B3B1F")]
     internal sealed class AspNetDiagnosticsEventSource : EventSource
     {
         /// <summary>
@@ -17,25 +17,37 @@ namespace Microsoft.AspNet.Diagnostics
         [Event(1, Message = "DiagnosticsHttpModule.{0}", Level = EventLevel.Informational)]
         public void DiagnosticsHttpModule(string callback)
         {
-            WriteEvent(1, callback);
+            if(IsEnabled())
+            {
+                WriteEvent(1, callback);
+            }
         }
 
         [Event(3, Message = "Activity started, Id='{0}'", Level = EventLevel.Informational)]
         public void ActivityStarted(string id)
         {
-            WriteEvent(3, id);
+            if (IsEnabled())
+            {
+                WriteEvent(3, id);
+            }
         }
 
         [Event(4, Message = "Activity stopped, Id='{0}', lost {1}", Level = EventLevel.Informational)]
         public void ActivityStopped(string id, bool lost = false)
         {
-            WriteEvent(4, id, lost);
+            if (IsEnabled())
+            {
+                WriteEvent(4, id, lost);
+            }
         }
 
         [Event(5, Message = "Failed to parse header {0}, value: '{1}'", Level = EventLevel.Error)]
         public void HeaderParsingFailure(string headerName, string headerValue)
         {
-            WriteEvent(5, headerName, headerValue);
+            if (IsEnabled())
+            {
+                WriteEvent(5, headerName, headerValue);
+            }
         }
     }
 }
