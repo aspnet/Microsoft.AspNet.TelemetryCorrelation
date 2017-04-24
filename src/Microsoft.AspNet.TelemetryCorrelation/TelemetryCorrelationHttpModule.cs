5f760d496b4a228a6ea15eeeb78fd4985e6bee01
Liudmila Molkova <lmolkova@microsoft.com>
2017-04-24 10:18:08 -0700

    if BeginRequest is never called, EndRequest may throw NullReferenceException

--- src/Microsoft.AspNet.TelemetryCorrelation/TelemetryCorrelationHttpModule.cs
+++ src/Microsoft.AspNet.TelemetryCorrelation/TelemetryCorrelationHttpModule.cs
@@ -50,22 +50,21 @@ namespace Microsoft.AspNet.TelemetryCorrelation
         {
             AspNetDiagnosticsEventSource.Log.TelemetryCorrelationHttpModule("Application_EndRequest");
 
-            // EndRequest does it's best effort to notify that request has ended
             var context = CurrentHttpContext;
-            var activity = (Activity)context.Items[ActivityHelper.ActivityKey];
-            var beginRequestWasCalled = (bool)context.Items[BeginCalledFlag];
 
-            // try to stop activity if it's in the Current stack
-            if (!ActivityHelper.StopAspNetActivity(activity, context))
+            // EndRequest does it's best effort to notify that request has ended
+            // BeginRequest has never been called
+            if (!context.Items.Contains(BeginCalledFlag))
             {
-                // Activity started by this module is not in the stack or BeginRequest has never been called
-                if (!beginRequestWasCalled)
-                {
-                    // Activity has never been started
-                    activity = ActivityHelper.CreateRootActivity(CurrentHttpContext);
-                    ActivityHelper.StopAspNetActivity(activity, context);
-                }
-                else
+                // Activity has never been started
+                var activity = ActivityHelper.CreateRootActivity(CurrentHttpContext);
+                ActivityHelper.StopAspNetActivity(activity, context);
+            }
+            else
+            {
+                var activity = (Activity)context.Items[ActivityHelper.ActivityKey];
+                // try to stop activity if it's in the Current stack
+                if (!ActivityHelper.StopAspNetActivity(activity, context))
                 {
                     // Activity we created was lost, let's report it
                     var lostActivity = CurrentHttpContext.Items[ActivityHelper.ActivityKey] as Activity;
