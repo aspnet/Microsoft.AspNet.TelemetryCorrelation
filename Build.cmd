55f776e4dc495d71bc986157b9be0877d90a6e4b
Liudmila Molkova <lmolkova@microsoft.com>
2017-05-08 23:03:53 -0700

    Fix Build script (#18)
    * Fix nuspec and Build script
    
    * minor review comments

--- Build.cmd
+++ Build.cmd
@@ -6,16 +6,14 @@ set EnableNuGetPackageRestore=true
 
 set logOptions=/flp:Summary;Verbosity=detailed;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
-REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (installed with VS2013) or later since .NET 4.0
-REM is not supported. Always quote the %MSBuild% value when setting the variable and never quote %MSBuild% references.
-set MSBuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+REM Find the most recent 32bit MSBuild.exe on the system. Require v14.0 (installed with VS2015) or later. Always quote the %MSBuild% value when setting the variable and never quote %MSBuild% references.
+set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 if not exist %MSBuild% (
   echo Could not find msbuild.exe. Please run this from a Visual Studio developer prompt
   goto BuildFail
 )
 
-%MSBuild% "%~dp0\Microsoft.AspNet.TelemetryCorrelation.msbuild" %logOptions% /v:minimal /maxcpucount /nodeReuse:false %*
+%MSBuild% "%~dp0\Microsoft.AspNet.TelemetryCorrelation.msbuild" %logOptions% /v:minimal /maxcpucount /nodeReuse:false /p:Configuration=Release /p:Platform=AnyCPU %*
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
