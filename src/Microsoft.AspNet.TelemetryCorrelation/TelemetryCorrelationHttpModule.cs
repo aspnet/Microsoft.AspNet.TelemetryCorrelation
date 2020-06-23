b32e9f8c22d1c6f76d01dda7984963f9457238fb
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-06-23 14:35:09 -0700

    Fixed to track end request from other modules.

--- src/Microsoft.AspNet.TelemetryCorrelation/TelemetryCorrelationHttpModule.cs
+++ src/Microsoft.AspNet.TelemetryCorrelation/TelemetryCorrelationHttpModule.cs
@@ -15,6 +15,13 @@ namespace Microsoft.AspNet.TelemetryCorrelation
     public class TelemetryCorrelationHttpModule : IHttpModule
     {
         private const string BeginCalledFlag = "Microsoft.AspNet.TelemetryCorrelation.BeginCalled";
+
+        // ServerVariable set only on rewritten HttpContext by URL Rewrite module.
+        private const string URLRewriteRewrittenRequest = "IIS_WasUrlRewritten";
+
+        // ServerVariable set on every request if URL module is registered in HttpModule pipeline.
+        private const string URLRewriteModuleVersion = "IIS_UrlRewriteModule";
+
         private static MethodInfo onStepMethodInfo = null;
 
         static TelemetryCorrelationHttpModule()
@@ -106,20 +113,21 @@ namespace Microsoft.AspNet.TelemetryCorrelation
             // BeginRequest has never been called
             if (!context.Items.Contains(BeginCalledFlag))
             {
-                // Exception happened before BeginRequest
-                if (context.Error != null)
+                // Rewrite: In case of rewrite, a new request context is created, called the child request, and it goes through the entire IIS/ASP.NET integrated pipeline.
+                // The child request can be mapped to any of the handlers configured in IIS, and it's execution is no different than it would be if it was received via the HTTP stack.
+                // The parent request jumps ahead in the pipeline to the end request notification, and waits for the child request to complete.
+                // When the child request completes, the parent request executes the end request notifications and completes itself.
+                // Do not create activity for parent request. Parent request has IIS_UrlRewriteModule ServerVariable with success response code.
+                // Child request contains an additional ServerVariable named - IIS_WasUrlRewritten.
+                // Track failed response activity: Different modules in the pipleline has ability to end the response. For example, authentication module could set HTTP 401 in OnBeginRequest and end the response.
+                if (context.Request.ServerVariables != null && context.Request.ServerVariables[URLRewriteRewrittenRequest] == null && context.Request.ServerVariables[URLRewriteModuleVersion] != null && context.Response.StatusCode == 200)
                 {
-                    // Activity has never been started
-                    ActivityHelper.CreateRootActivity(context, ParseHeaders);
+                    trackActivity = false;
                 }
                 else
                 {
-                    // Rewrite: In case of rewrite, a new request context is created, called the child request, and it goes through the entire IIS/ASP.NET integrated pipeline.
-                    // The child request can be mapped to any of the handlers configured in IIS, and it's execution is no different than it would be if it was received via the HTTP stack.
-                    // The parent request jumps ahead in the pipeline to the end request notification, and waits for the child request to complete.
-                    // When the child request completes, the parent request executes the end request notifications and completes itself.
-                    // Ignore creating root activity for parent request as control got transferred from rewrite module to EndRequest with no request flow.
-                    trackActivity = false;
+                    // Activity has never been started
+                    ActivityHelper.CreateRootActivity(context, ParseHeaders);
                 }
             }
 
